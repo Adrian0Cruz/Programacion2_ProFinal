15ff8f4bdbbf564a9b22cb35ae3cbdde160645d8
Jesús Hernández <jesusalbertohd15@gmail.com>
2024-05-14 00:05:07 -0500

    I changed the name of the classes

--- src/main/java/com/mycompany/programacion2_profinal/RegisterController.java
+++ src/main/java/com/mycompany/programacion2_profinal/RegisterController.java
@@ -3,10 +3,10 @@ package com.mycompany.programacion2_profinal;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class RegisterController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        App.setRoot("Login");
     }
 }
