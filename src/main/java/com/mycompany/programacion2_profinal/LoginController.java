0262dc2862ea6c3d9541e291e0eaef6b85c94d41
Jesús Hernández <jesusalbertohd15@gmail.com>
2024-05-14 08:39:50 -0500

    i avanced in view login

--- src/main/java/com/mycompany/programacion2_profinal/LoginController.java
+++ src/main/java/com/mycompany/programacion2_profinal/LoginController.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class LoginController {
-
+    @FXML
+    private void Exit() throws IOException {
+        System.exit ( 0 );
+    }
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("Register");
